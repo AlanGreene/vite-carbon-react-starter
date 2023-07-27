8e4c69e130b59705da290976e90c16210fafcea2
Alan Greene <github.com@alangreene.net>
2023-07-27 19:12:50 +0100

    Bootstrap the project
    Create new Vite + React project using:
    `npm create vite@latest vite-carbon-react-starter -- --template react`
    
    Install Carbon + SASS and add example usage including styles

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@carbon/react';
+
+export default function App() {
+  return (
+    <div>
+      <Button>Hello world</Button>
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './index.scss';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
